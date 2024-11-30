dcc87ba87e09efdc4f9f59972b1d7e8d5273ec28
MISHRAJII123 <ccaddnew@gmail.com>
2024-11-30 19:58:07 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -52,7 +52,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/MISHRAJII123/RADHAQUEEN",
+    "https://github.com/MISHRAJII123/EXTRAPLUGIN",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
