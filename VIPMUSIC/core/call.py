d09da76154779160449b5c26b9b459d87a5e6781
MISHRAJII123 <ccaddnew@gmail.com>
2024-12-06 15:48:00 +0530

    Update call.py

--- VIPMUSIC/core/call.py
+++ VIPMUSIC/core/call.py
@@ -75,7 +75,7 @@ async def _clear_(chat_id):
     await remove_active_chat(chat_id)
 
     await app.send_message(
-        chat_id, f"◉🎧𝗦𝗢𝗡𝗚 𝗘𝗡𝗗𝗘𝗗 𝗜𝗡 𝗩𝗖 𝗕𝗔𝗕𝗬🎧◉"
+        chat_id, f"❎ 𝐐ᴜᴇᴜᴇ 𝐈s 𝐄ᴍᴘᴛʏ 𝐒ᴏ 𝐋ᴇғᴛ 𝐅ʀᴏᴍ 𝐕ᴄ❗..."
     )
 
 
